81d4df108c93df0dafc2eb91ae3ec4d57556fb26
SIMRAN SAINI <91106038+1simransaini@users.noreply.github.com>
2021-10-14 22:19:39 +0530

    Q2

---
+++ Milestone-1/Print.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{    
+    int number;
+
+    cout << "Enter an integer: ";
+    cin >> number;
+
+    cout << "Number entered by the user" <<endl<<number;    
+    return 0;
+}
+ /*Enter an integer: 108
+ Number entered by the user
+ 108*/
