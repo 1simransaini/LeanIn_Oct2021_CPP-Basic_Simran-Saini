c4bf8e2e672fc63d7810be24572d03ee5cf2cbb1
SIMRAN SAINI <91106038+1simransaini@users.noreply.github.com>
2021-10-15 11:25:11 +0530

    Q4

---
+++ Milestone-1/print_sum.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+ 
+ int main()
+ {
+     int a,b,c,d;
+     
+     cout<< "Enter the value of a ";
+     
+     cin>>a;
+     
+     cout<< "Enter the value of b ";
+     
+     cin>>b;
+     
+     cout<< "Enter the value of c ";
+     
+     cin>>c;
+     
+     cout<< "Enter the value of d ";
+     
+     cin>>d;  
+
+     cout<<"Sum of the Numbers "<<(a+b+c+d)<<endl;
+     
+     return 0;
+ }
+/*Enter the value of a 5
+Enter the value of b 5
+Enter the value of c 5
+Enter the value of d 5
+Sum of the Numbers 20*/
