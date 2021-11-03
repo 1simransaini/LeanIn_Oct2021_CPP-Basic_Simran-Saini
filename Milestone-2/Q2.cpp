dbf8896de304e29b3dd54f60737ce6585e987493
SIMRAN SAINI <91106038+1simransaini@users.noreply.github.com>
2021-11-03 11:38:23 +0530

    Q2

---
+++ Milestone-2/Q2.cpp
@@ -0,0 +1,33 @@
+/* C++ program to print nos. from 1 to 50 that are divisible by 3*/
+
+#include<iostream>
+
+using namespace std;
+ 
+ int main()
+ {
+     for(int i=1;i<=50;i++)
+     {
+     if(i%3==0)
+     cout<<i<<endl;
+     }
+     
+     return 0;
+ }
+/*
+3
+6
+9
+12
+15
+18
+21
+24
+27
+30
+33
+36
+39
+42
+45
+48 */
