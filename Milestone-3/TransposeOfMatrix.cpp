9d37f29f20732dde6409b0c6b5465c7bd1e6d54e
SIMRAN SAINI <91106038+1simransaini@users.noreply.github.com>
2021-11-11 19:49:22 +0530

    Q4

---
+++ Milestone-3/TransposeOfMatrix.cpp
@@ -0,0 +1,72 @@
+//C++ program Find Transpose of a Matrix
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   int a[10][10], transpose[10][10], row, column, i, j;
+
+   cout << "Enter rows and columns of matrix: ";
+   cin >> row >> column;
+
+   cout << "\nEnter elements of matrix: " << endl;
+
+   // Storing matrix elements
+   for (int i = 0; i < row; ++i) {
+      for (int j = 0; j < column; ++j) {
+         cout << "Enter element a" << i + 1 << j + 1 << ": ";
+         cin >> a[i][j];
+      }
+   }
+
+   // Printing the a matrix
+   cout << "\nEntered Matrix: " << endl;
+   for (int i = 0; i < row; ++i) {
+      for (int j = 0; j < column; ++j) {
+         cout << " " << a[i][j];
+         if (j == column - 1)
+            cout << endl << endl;
+      }
+   }
+
+   // Computing transpose of the matrix
+   for (int i = 0; i < row; ++i)
+      for (int j = 0; j < column; ++j) {
+         transpose[j][i] = a[i][j];
+      }
+
+   // Printing the transpose
+   cout << "\nTranspose of Matrix: " << endl;
+   for (int i = 0; i < column; ++i)
+      for (int j = 0; j < row; ++j) {
+         cout << " " << transpose[i][j];
+         if (j == row - 1)
+            cout << endl << endl;
+      }
+
+   return 0;
+}
+/*Enter rows and columns of matrix: 2
+3
+
+Enter elements of matrix: 
+Enter element a11: 1
+Enter element a12: 2
+Enter element a13: 3
+Enter element a21: 4
+Enter element a22: 5
+Enter element a23: 6
+
+Entered Matrix: 
+ 1 2 3
+
+ 4 5 6
+
+
+Transpose of Matrix: 
+ 1 4
+
+ 2 5
+
+ 3 6
+*/
