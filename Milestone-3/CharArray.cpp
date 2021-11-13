c0b382d5831d1ad83dd5367c21dda605f1091e90
SIMRAN SAINI <91106038+1simransaini@users.noreply.github.com>
2021-11-13 15:50:05 +0530

    Q1

---
+++ Milestone-3/CharArray.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char name[100];
+
+    cout << "Enter a string: ";
+    cin >> name;
+    cout << "You entered or output: " << name << endl;
+
+     return 0;
+}
+/*Enter a string: HELLO
+You entered or output: HELLO*/
