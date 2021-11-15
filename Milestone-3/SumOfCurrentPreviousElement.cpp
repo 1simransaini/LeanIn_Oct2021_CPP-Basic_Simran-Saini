f3ea1f4ffd4e7ed4eac190ec0a5d608ab3b16c14
SIMRAN SAINI <91106038+1simransaini@users.noreply.github.com>
2021-11-15 12:04:41 +0530

    Q7

--- Milestone-3/SumOfCurrentPreviousElement.cpp
+++ Milestone-3/SumOfCurrentPreviousElement.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+ 
+using namespace std;
+int main() {
+   int i,n,sum=0;
+   int arr[]={20,10,5,3,80,55};
+   //int Updated_arr[n];
+   n=6;
+   for(i=0;i<n;i++) {
+      sum+=arr[i];
+      cout<<sum<<"   ";
 
+   }
+   
+   return 0;
+}
+//20   30   35   38   118   173 
