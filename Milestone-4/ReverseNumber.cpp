ce96a96191750aa2ac968e078ab33741d5027ddb
SIMRAN SAINI <91106038+1simransaini@users.noreply.github.com>
2021-11-26 08:45:57 +0530

    Q1

---
+++ Milestone-4/ReverseNumber.cpp
@@ -0,0 +1,21 @@
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+int n, reverse=0, rem;    
+cout<<"Enter a number: ";    
+cin>>n;    
+  while(n!=0)    
+  {    
+     rem=n%10;      
+     reverse=reverse*10+rem;    
+     n/=10;    
+  }    
+ cout<<"Reversed Number: "<<reverse<<endl;     
+return 0;  
+}  
+
+/*
+Enter a number: 123456789
+Reversed Number: 987654321
+*/
