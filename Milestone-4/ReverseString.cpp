645cbbdb715fd1e1cd70cc06859d42f45319642e
SIMRAN SAINI <91106038+1simransaini@users.noreply.github.com>
2021-11-28 11:22:42 +0530

    Q9

---
+++ Milestone-4/ReverseString.cpp
@@ -0,0 +1,31 @@
+//C++ program to reverse a string 
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    char str[100], temp;
+    int size = 0, i = 0;
+
+    cout << "Enter a string:";
+    cin.getline(str, 100);
+
+    while (str[size] != '\0') {
+        size++;
+    }
+
+    size = size - 1;
+    while (i < size) {
+        temp = str[i];
+        str[i] = str[size];
+        str[size] = temp;
+        size--;
+        i++;
+    }
+    cout << "After reverse string:" << str;
+
+    return 0;
+}
+/*Enter a string:SIMRAN SAINI MILESTONE 4
+After reverse string:4 ENOTSELIM INIAS NARMIS
+*/
