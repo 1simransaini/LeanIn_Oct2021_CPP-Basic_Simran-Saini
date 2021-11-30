5174213ca4376611ff25795efd4282c0d1dfb699
SIMRAN SAINI <91106038+1simransaini@users.noreply.github.com>
2021-11-30 09:32:26 +0530

    Q4(C)

---
+++ Milestone-4/InsertionSort.cpp
@@ -0,0 +1,45 @@
+//C++ program insertion sort
+#include<iostream>
+using namespace std;
+void display(int *array, int size) {
+   for(int i = 0; i<size; i++)
+      cout << array[i] << " ";
+   cout << endl;
+}
+void insertionSort(int *array, int size) {
+   int key, j;
+   for(int i = 1; i<size; i++) {
+      key = array[i];
+      j = i;
+      while(j > 0 && array[j-1]>key) {
+         array[j] = array[j-1];
+         j--;
+      }
+      array[j] = key;  
+   }
+}
+int main() {
+   int n;
+   cout << "Enter the number of elements: ";
+   cin >> n;
+   int arr[n];    //array with given number of elements
+   cout << "Enter elements:" << endl;
+   for(int i = 0; i<n; i++) {
+      cin >> arr[i];
+   }
+   cout << "Array before Sorting: ";
+   display(arr, n);
+   insertionSort(arr, n);
+   cout << "Array after Sorting: ";
+   display(arr, n);
+}
+/*Enter the number of elements: 5
+Enter elements:
+111
+120
+99
+882  
+122
+Array before Sorting: 111 120 99 882 122 
+Array after Sorting: 99 111 120 122 882 
+*/
